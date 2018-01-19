30123851949c2e9c169ba2388f2c5b9d6e4b0d21
U-DESKTOP-3S1A2JN\Johann <11635589+j-l5662@users.noreply.github.com>
2018-01-18 20:05:28 -0500

    Exercise 1/18

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,10 +15,14 @@
  */
 package com.example.android.datafrominternet;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,24 +42,38 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
     // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
-    // TODO (2) Create a menu xml called 'main.xml' in the res->menu folder
-    // TODO (3) Add one menu item to your menu
-    // TODO (4) Give the menu item an id of @+id/action_search
-    // TODO (5) Set the orderInCategory to 1
-    // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
-    // TODO (7) Set the title to the search string ("Search") from strings.xml
+    // done (2) Create a menu xml called 'main.xml' in the res->menu folder
+    // done (3) Add one menu item to your menu
+    // done (4) Give the menu item an id of @+id/action_search
+    // done (5) Set the orderInCategory to 1
+    // done (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
+    // done (7) Set the title to the search string ("Search") from strings.xml
     // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
 
 
-    // TODO (8) Override onCreateOptionsMenu
-    // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
-    // TODO (10) Return true to display your menu
+    // done (8) Override onCreateOptionsMenu
+    // done (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
+    // done (10) Return true to display your menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int menuID = item.getItemId();
+        if(menuID == R.id.action_search){
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
 
-    // TODO (11) Override onOptionsItemSelected
-    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+    // done (11) Override onOptionsItemSelected
+    // done (12) Within onOptionsItemSelected, get the ID of the item that was selected
+    // done (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
+    // done (14) Don't forgot to call .show() on your Toast
+    // done (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
 }
--- Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -20,7 +20,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
@@ -36,10 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mSearchResultsTextView;
 
-    // TODO (12) Create a variable to store a reference to the error message TextView
-
-    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
-
+    // done (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorMessage;
+    // done (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mProgessBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
-        // TODO (13) Get a reference to the error TextView using findViewById
-
-        // TODO (25) Get a reference to the ProgressBar using findViewById
+        // done (13) Get a reference to the error TextView using findViewById
+        mErrorMessage = (TextView) findViewById(R.id.tv_error_message_display);
+        // done (25) Get a reference to the ProgressBar using findViewById
+        mProgessBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 
     /**
@@ -68,14 +71,24 @@ public class MainActivity extends AppCompatActivity {
         new GithubQueryTask().execute(githubSearchUrl);
     }
 
-    // TODO (14) Create a method called showJsonDataView to show the data and hide the error
-
-    // TODO (15) Create a method called showErrorMessage to show the error and hide the data
-
+    // done (14) Create a method called showJsonDataView to show the data and hide the error
+    private void showJsonData(){
+        mErrorMessage.setVisibility(View.INVISIBLE);
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+    // done (15) Create a method called showErrorMessage to show the error and hide the data
+    private void showErrorMessage(){
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.VISIBLE);
+    }
     public class GithubQueryTask extends AsyncTask<URL, Void, String> {
 
-        // TODO (26) Override onPreExecute to set the loading indicator to visible
-
+        // done (26) Override onPreExecute to set the loading indicator to visible
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            mProgessBar.setVisibility(View.VISIBLE);
+        }
         @Override
         protected String doInBackground(URL... params) {
             URL searchUrl = params[0];
@@ -90,12 +103,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String githubSearchResults) {
-            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            // done (27) As soon as the loading is complete, hide the loading indicator
+            mProgessBar.setVisibility(View.INVISIBLE);
             if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                // TODO (17) Call showJsonDataView if we have valid, non-null results
+                // done (17) Call showJsonDataView if we have valid, non-null results
+                showJsonData();
                 mSearchResultsTextView.setText(githubSearchResults);
+
             }
-            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
+            else{
+                showErrorMessage();
+            }
+            // done (16) Call showErrorMessage if the result is null in onPostExecute
         }
     }
 
